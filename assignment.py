b78f58ac4071d3d2227e5a86a52c48037accd90e
Lucas-Vu-8 <Lucas.vu@stu.ocsb.ca>
2024-10-22 13:43:56 +0000

    y

--- assignment.py
+++ assignment.py
@@ -1,18 +1,28 @@
 import math
 
 def calculate_circle_properties():
-    diameter = input("Enter the diameter of the circle: ")
-    try:
-        diameter = float(diameter)
-    except ValueError:
-        print("Error: Please enter a valid number.")
-    if diameter < 0:
-        print("Error: Diameter cannot be negative. Please enter a valid value.")
-        return 
-    radius = diameter / 2
-    circumference = math.pi * diameter
-    area = math.pi * radius ** 2
-    print(circumference)
-    print(area)
+    while True:
+        diameter = input("Enter the diameter of the circle: ")
+        try:
+            diameter = float(diameter)
+        except ValueError:
+            print("Error: Please enter a valid number.")
+            continue 
+
+        if diameter < 0:
+            print("Error: Diameter cannot be negative. Please enter a valid value.")
+            continue  
+
+        radius = diameter / 2
+        circumference = math.pi * diameter
+        area = math.pi * radius ** 2
+
+        print(circumference)
+        print(area)
+        
+        another = input("Would you like to calculate another circle").lower()
+        if another != 'yes':
+            print("bye")
+            break
 
 calculate_circle_properties()
